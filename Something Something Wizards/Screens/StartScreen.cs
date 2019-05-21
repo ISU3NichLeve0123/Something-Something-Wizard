93bb1b7bfa0500d0d8bffe4e62aee102325dbb5f
nichleve097 <nichleve097@SCS-D-360QHR>
2019-05-21 10:05:48 -0400

    Add project files.

---
+++ Something Something Wizards/Screens/StartScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Something_Something_Wizards
+{
+    public partial class StartScreen : UserControl
+    {
+        public StartScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
